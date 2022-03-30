654d2b3cb183f19265fc94ea0e4dc3bffc94ad62
JoaoLCordeiro <jlc@inf.ufpr.br>
2022-03-30 14:40:52 -0300

    dei uma comentada

--- trabalho-1.c
+++ trabalho-1.c
@@ -3,51 +3,51 @@
 
 #define MAX 1000
 
-struct t_entrada{
-    int     n_var;
-    char*   funcao;
-    double* valores_ini;
-    double  epslon;
-    int     iteracoes;
+struct t_entrada{			//tipo entrada: armazena os dados da entrada atual
+	int		n_var;			//número de variáveis
+	char*	funcao;			//string da funcao (TALVEZ ALTERAR AQUI)
+	double*	valores_ini;	//vetor de valores iniciais para as incógnitas
+	double	epslon;			//tolerância epslon
+	int		iteracoes;		//número máximo de iterações
 } typedef t_entrada;
 
-int le_funcao(t_entrada* entrada){
-    scanf("%d\n", &(entrada->n_var));
+int le_entrada(t_entrada* entrada){					//função que lê a entrada
+	scanf("%d\n", &(entrada->n_var));												//lê o número de incógnitas
 
-    entrada->funcao = (char *) malloc ((MAX+1)*sizeof(char));
-    fgets(entrada->funcao, MAX, stdin);
+	entrada->funcao = (char *) malloc ((MAX+1)*sizeof(char));						//malloca e lê a string da funcao
+	fgets(entrada->funcao, MAX, stdin);
 
-    entrada->valores_ini = (double *) malloc (entrada->n_var * sizeof(double));
-    for (int i = 0; i < entrada->n_var ; i++){
-        scanf("%lf ",&(entrada->valores_ini[i]));
-    }
+	entrada->valores_ini = (double *) malloc (entrada->n_var * sizeof(double));		//malloca e lê um vetor de valores iniciais para as incógnitas
+	for (int i = 0; i < entrada->n_var ; i++){
+		scanf("%lf ",&(entrada->valores_ini[i]));
+	}
 
-    entrada->epslon;
-    scanf("%lf\n", &(entrada->epslon));
+	entrada->epslon;
+	scanf("%lf\n", &(entrada->epslon));												//lê o número para a tolerância epslon
 
-    entrada->iteracoes;
-    scanf("%d\n", &(entrada->iteracoes));
+	entrada->iteracoes;
+	scanf("%d\n", &(entrada->iteracoes));											//lê o número máximo de iterações
 }
 
-void imprime_entrada (t_entrada *entrada){
-    printf ("%d\n", entrada->n_var);
-    printf ("%s", entrada->funcao);
-    for (int i = 0; i < entrada->n_var ; i++)
-        printf ("%lf ", entrada->valores_ini[i]);
-    printf ("\n");
-    printf ("%lf\n", entrada->epslon);
-    printf ("%d\n", entrada->iteracoes);
+void imprime_entrada (t_entrada *entrada){				//funcao só pra debuggar :)
+	printf ("%d\n", entrada->n_var);
+	printf ("%s", entrada->funcao);
+	for (int i = 0; i < entrada->n_var ; i++)
+		printf ("%lf ", entrada->valores_ini[i]);
+	printf ("\n");
+	printf ("%lf\n", entrada->epslon);
+	printf ("%d\n", entrada->iteracoes);
 }
 
 int main(){
-    t_entrada* entrada_atual = (t_entrada *) malloc (sizeof(t_entrada));
+	t_entrada* entrada_atual = (t_entrada *) malloc (sizeof(t_entrada));
 
-    if (le_funcao(entrada_atual) != 0)
-        //tratar erros na leitura de funcoes
-    
-    imprime_entrada (entrada_atual);
+	if (le_entrada(entrada_atual) != 0)
+		//tratar erros na leitura de funcoes
+	
+	imprime_entrada (entrada_atual);
 
-    //if (free_funcoes() != 0)
-        //tratar erros nos frees das variaveis das funcoes
-    return 1;
+	//if (free_funcoes() != 0)
+		//tratar erros nos frees das variaveis das funcoes
+	return 1;
 }
