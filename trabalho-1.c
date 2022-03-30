57661e79eb4412c1757a858c474f42c5f9d8793c
JoaoLCordeiro <jlc@inf.ufpr.br>
2022-03-30 14:17:38 -0300

    funcao de leitura de funcao praticamente pronta

--- trabalho-1.c
+++ trabalho-1.c
@@ -1,10 +1,53 @@
-git#include <std.io.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int le_funcao(){
+#define MAX 1000
 
+struct t_entrada{
+    int     n_var;
+    char*   funcao;
+    double* valores_ini;
+    double  epslon;
+    int     iteracoes;
+} typedef t_entrada;
+
+int le_funcao(t_entrada* entrada){
+    scanf("%d\n", &(entrada->n_var));
+
+    entrada->funcao = (char *) malloc ((MAX+1)*sizeof(char));
+    fgets(entrada->funcao, MAX, stdin);
+
+    entrada->valores_ini = (double *) malloc (entrada->n_var * sizeof(double));
+    for (int i = 0; i < entrada->n_var ; i++){
+        scanf("%lf ",&(entrada->valores_ini[i]));
+    }
+
+    entrada->epslon;
+    scanf("%lf\n", &(entrada->epslon));
+
+    entrada->iteracoes;
+    scanf("%d\n", &(entrada->iteracoes));
+}
+
+void imprime_entrada (t_entrada *entrada){
+    printf ("%d\n", entrada->n_var);
+    printf ("%s", entrada->funcao);
+    for (int i = 0; i < entrada->n_var ; i++)
+        printf ("%lf ", entrada->valores_ini[i]);
+    printf ("\n");
+    printf ("%lf\n", entrada->epslon);
+    printf ("%d\n", entrada->iteracoes);
 }
 
 int main(){
-    if (le_funcao() != 0)
+    t_entrada* entrada_atual = (t_entrada *) malloc (sizeof(t_entrada));
+
+    if (le_funcao(entrada_atual) != 0)
         //tratar erros na leitura de funcoes
+    
+    imprime_entrada (entrada_atual);
+
+    //if (free_funcoes() != 0)
+        //tratar erros nos frees das variaveis das funcoes
+    return 1;
 }
