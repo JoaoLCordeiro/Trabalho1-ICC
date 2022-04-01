518385884ca0fae4f34b305c57e3cdbdd56a9dd5
JoaoLCordeiro <jlc@inf.ufpr.br>
2022-04-01 12:30:05 -0300

    começando a usar libmatheval

--- trabalho-1.c
+++ trabalho-1.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <matheval.h>
+#include <assert.h>
+#include <string.h>
 
 #define MAX 1000
 
 struct t_entrada{			//tipo entrada: armazena os dados da entrada atual
 	int		n_var;			//número de variáveis
-	char*	funcao;			//string da funcao (TALVEZ ALTERAR AQUI)
+	void*	funcao;			//string da funcao (TALVEZ ALTERAR AQUI)
 	double*	valores_ini;	//vetor de valores iniciais para as incógnitas
 	double	epslon;			//tolerância epslon
 	int		iteracoes;		//número máximo de iterações
 } typedef t_entrada;
 
 int le_entrada(t_entrada* entrada){					//função que lê a entrada
-	scanf("%d\n", &(entrada->n_var));												//lê o número de incógnitas
+	if ((scanf("%d\n", &(entrada->n_var))) == EOF)									//lê o número de incógnitas
+		return (-1);		//erro na leitura: o arquivo acabou
 
-	entrada->funcao = (char *) malloc ((MAX+1)*sizeof(char));						//malloca e lê a string da funcao
-	fgets(entrada->funcao, MAX, stdin);
+	char* funcao_string = (char *) malloc ((MAX+1)*sizeof(char));					//malloca e lê a string da funcao
+	fgets(funcao_string, MAX, stdin);
+	strtok(funcao_string, "\n");													//tira o \n do string para acertar a sintaxe para o evaluator
+	entrada->funcao	= evaluator_create(funcao_string);								//cria a funcao e poe em funcao
+	free (funcao_string);
+
+	if (entrada->funcao == NULL)
+		return (-2);		//erro na evaluacao: erro de sintaxe na funcao
+
+	printf ("%s", funcao_string);
 
 	entrada->valores_ini = (double *) malloc (entrada->n_var * sizeof(double));		//malloca e lê um vetor de valores iniciais para as incógnitas
 	for (int i = 0; i < entrada->n_var ; i++){
@@ -27,6 +39,7 @@ int le_entrada(t_entrada* entrada){					//função que lê a entrada
 
 	entrada->iteracoes;
 	scanf("%d\n", &(entrada->iteracoes));											//lê o número máximo de iterações
+	return (0);
 }
 
 void imprime_entrada (t_entrada *entrada){				//funcao só pra debuggar :)
@@ -40,14 +53,13 @@ void imprime_entrada (t_entrada *entrada){				//funcao só pra debuggar :)
 }
 
 int main(){
-	t_entrada* entrada_atual = (t_entrada *) malloc (sizeof(t_entrada));
+	t_entrada* entrada_atual 	= (t_entrada *) malloc (sizeof(t_entrada));
 
-	if (le_entrada(entrada_atual) != 0)
-		//tratar erros na leitura de funcoes
-	
-	imprime_entrada (entrada_atual);
+	while (le_entrada(entrada_atual) == 0){
+		
+		imprime_entrada (entrada_atual);
+		//free_entrada(); dar free na entrada para a próxima
+	}
 
-	//if (free_funcoes() != 0)
-		//tratar erros nos frees das variaveis das funcoes
 	return 1;
 }
