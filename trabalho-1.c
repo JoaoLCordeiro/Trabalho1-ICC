efbf5f7229fa93c78e4d9ecca1dc4518250069c8
JoaoLCordeiro <jlc@inf.ufpr.br>
2022-04-01 15:40:43 -0300

    começando o newton padrão

--- trabalho-1.c
+++ trabalho-1.c
@@ -42,6 +42,69 @@ int le_entrada(t_entrada* entrada){					//função que lê a entrada
 	return (0);
 }
 
+void imprime_v_m (void** v_deriv, void*** m_deriv, int n_vars){
+	for (int i = 0 ; i < n_vars ; i++){
+		printf ("  f'[%d](x^(i))) = %s\n", i, evaluator_get_string (v_deriv[i]));
+	}
+
+	for (int i = 0 ; i < n_vars ; i++){
+		for (int j = 0 ; j < n_vars ; j++){
+			printf ("  f''[%d][%d](x^(i))) = %s\t", i, j, evaluator_get_string (m_deriv[i][j]));
+		}
+		printf("\n");
+	}
+}
+
+void newton_padrao (t_entrada* entrada){
+	void* funcao 	= entrada->funcao;
+	char** v_vars;
+	int n_vars;
+	evaluator_get_variables (funcao, &v_vars, &n_vars);					//pega as informações da funcao atual
+
+	void**  v_deriv		= (void **)  malloc(n_vars*sizeof(void *));		//malloca um vetor de funcoes derivadas primeiras
+	void*** m_deriv		= (void ***) malloc(n_vars*sizeof(void **));	//malloca uma matriz de funcoes derivadas segundas
+	for (int i = 0 ; i < n_vars ; i++)
+		m_deriv[i]		= (void **)  malloc(n_vars*sizeof(void *));
+
+	void* d_funcao;
+	void* dd_funcao;
+
+	for (int i = 0 ; i < n_vars ; i++){									//enche o vetor v_deriv de derivadas
+		d_funcao 	= evaluator_derivative(funcao, v_vars[i]);			//parciais primeiras
+		v_deriv[i] 	= d_funcao;											//gradiente
+	}
+
+	for (int i = 0 ; i < n_vars ; i++){									//enche a matriz m_deriv de derivadas
+		d_funcao = v_deriv[i];											//parciais segundas
+		for (int j = 0 ; j < n_vars ; j++){
+			dd_funcao 		= evaluator_derivative(d_funcao, v_vars[j]);
+			m_deriv[i][j] 	= dd_funcao;								//hessiana
+		}
+	}
+
+	double*  v_f_iteracao	= (double *)  malloc (n_vars*sizeof(double));	//guarda valores atuais do gradiente para os valroes de x atuais
+	double** m_f_iteracao	= (double **) malloc (n_vars*sizeof(double*));	//guarda valores atuais da hessiana para os valroes de x atuais
+	for (int i = 0 ; i < n_vars ; i++)
+		v_f_iteracao[i]		= (double *)  malloc (n_vars*sizeof(double));
+
+	double* v_delta		= (double *) malloc (n_vars*sizeof(double));		//vetor delta que será calculado
+	double* v_X_atual	= (double *) malloc (n_vars*sizeof(double));		//vetor que guarda os valores de x atuais
+	for (int i = 0 ; i < n_vars ; i++)
+		v_X_atual[i]	= entrada->valores_ini[i];
+
+	while (/*CONDIÇÃO DE PARADA*/ 1){
+		for (int i = 0 ; i < n_vars ; i++){
+			v_f_iteracao[i]	= evaluator_evaluate(v_deriv[i], n_vars, v_vars, v_X_atual);
+			for (int j = 0 ; j < n_vars ; j++){
+				m_f_iteracao[i][j] = evaluator_evaluate(m_deriv[i][j], n_vars, v_vars, v_X_atual);
+			}
+		}
+	}
+
+	//imprime_v_m (v_deriv, m_deriv, n_vars); funcao de debug: imprime as funcoes derivadas encontradas
+	//dar free no final nesses vetores/matrizes
+}
+
 void imprime_entrada (t_entrada *entrada){				//funcao só pra debuggar :)
 	printf ("%d\n", entrada->n_var);
 	printf ("%s", entrada->funcao);
@@ -55,9 +118,14 @@ void imprime_entrada (t_entrada *entrada){				//funcao só pra debuggar :)
 int main(){
 	t_entrada* entrada_atual 	= (t_entrada *) malloc (sizeof(t_entrada));
 
-	while (le_entrada(entrada_atual) == 0){
+	int cont = 0;
+
+	while ((le_entrada(entrada_atual) == 0)&&(cont < 10)){
 		
-		imprime_entrada (entrada_atual);
+		//imprime_entrada (entrada_atual);
+
+		newton_padrao (entrada_atual);
+		cont++;
 		//free_entrada(); dar free na entrada para a próxima
 	}
 
